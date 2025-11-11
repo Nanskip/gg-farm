2f9b30fd2566035064870b7029b47977ec07ead0
Nanskip <nanskip.oof@gmail.com>
2025-11-11 09:55:16 +0100

    Updated CropManager.ts and made it AirshipSingleton

--- Assets/Resources/Global Scripts/CropManager.ts
+++ Assets/Resources/Global Scripts/CropManager.ts
@@ -1,16 +1,42 @@
+import { Game } from "@Easy/Core/Shared/Game";
 import { NetworkSignal } from "@Easy/Core/Shared/Network/NetworkSignal";
 
-export default class CropManager extends AirshipBehaviour {
+export default class CropManager extends AirshipSingleton {
 	// CropManager is a singleton that manages crops.
 	// It is responsible for creating crops, updating them, and destroying them.
 	// All the operations are done on the server and just syncing with the clients.
 
 	// A signal to load new land on both sides.
 	// On client called when player picks land, on server called when land is created and sends data to clients.
-	private SIGNAL_LOAD_LAND: NetworkSignal = new NetworkSignal<{}>("LOAD_LAND");
+	private SIGNAL_LOAD_LAND = new NetworkSignal<{data: String}>("LOAD_LAND");
 
-	@Server()
 	override Start(): void {
-		print("SERVER: CropManager initialized.");
+		// -- SERVER --
+		if (Game.IsServer()) {
+			print("SERVER: CropManager initialized.");
+
+			this.ConnectServerSignals();
+		}
+
+		// -- CLIENT --
+		if (Game.IsClient()) {
+			print("CLIENT: CropManager initialized.");
+		}
+	}
+
+	public ConnectServerSignals(): void {
+		this.SIGNAL_LOAD_LAND.server.OnClientEvent((player, data) => {
+			print("SERVER: CropManager received signal.");
+			print("SERVER: Data: " + data.data);
+		});
+	}
+
+	public LoadLand(data: String): void {
+		if (Game.IsServer()) {
+
+		} else if (Game.IsClient()) {
+			//this.SIGNAL_LOAD_LAND.client.FireServer({data});
+			this.SIGNAL_LOAD_LAND.client.FireServer({data: "NEW"});
+		}
 	}
 }
--- Assets/Resources/Land/CropTile.ts
+++ Assets/Resources/Land/CropTile.ts
@@ -1,14 +1,15 @@
+import CropManager from "Resources/Global Scripts/CropManager";
+
 export default class CropTile extends AirshipBehaviour {
 	// Mainly crop tile is a tile with some data, using updater to create stuff if needed.
 	// Crop tiles should be managed with CropManager, which is a singleton that communicates with the server.
 
-	public CropManager: GameObject;
-
-	private _manager: GameObject;
 	private _IS_PLANTED: boolean = false;
 	private _DATA: String = "";
 
 	override Start(): void {
 		//this._manager = this.CropManager.GetAirshipComponent<CropManager>();
+
+		//CropManager.Get().
 	}
 }
